4490baa3df8e542024863aad86fc7d51293c731d
EC2 Default User <ec2-user@ip-172-31-6-52.us-east-2.compute.internal>
2020-05-20 16:25:14 +0000

    いいね機能Controller部完成

--- app/Http/Controllers/goodController.php
+++ app/Http/Controllers/goodController.php
@@ -13,15 +13,16 @@ class goodController extends Controller
     {
         //レコードの検索（条件：user_idとspeech_idが両方とも一致すること）
         $user = Auth::User();
-        $good_state = Good::where('speechID',$request->speechID);
-        $good_state -> where('user_id',$user->id);
+        $good_state = Good::where('speechID',$request->speechID)
+            -> where('user_id',$user->id)
+            -> first();
         $good_state_exists = $good_state -> exists();
         //レコードが存在する場合の処理
         if ($good_state_exists) {
             //条件:DBに登録されている状態と送信された状態が同じか違うか
             //式１:対象のレコードを削除
             //式２:対象のレコードの状態を更新
-            $good_state["status"] == $request->state ? $good_state->delete() : $good_state->update(['status' => $request->state]);
+            $good_state->status == $request->state ? $good_state->delete() : $good_state->update(['status' => $request->state]);
         }
         //レコードが存在しない場合の処理
         else {
@@ -34,7 +35,7 @@ class goodController extends Controller
             ]);
         }
         
-        return redirect();
+        return redirect()->route('detail',['issueID' => $request->issueID]);
         
     }
 }
