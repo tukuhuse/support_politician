d290d99d103688ee7662b0406bfa438fd51c8de2
EC2 Default User <ec2-user@ip-172-31-6-52.us-east-2.compute.internal>
2020-06-24 15:54:28 +0000

    jqueryでの要素取得まで

--- public/js/good.js
+++ public/js/good.js
@@ -93,38 +93,12 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-/*
-$(function() {
-    $('#btn-good').click(function(e){
-        //フォームが送信された時、デフォルトの通信を止める
-        e.stopPropagation();
-        
-        //通信を行う
-        $.ajax({
-            type: 'POST',
-            url: '#',
-            data: {
-                'speechID': $speechID,
-                'status': $status,
-                'legislator_id': $legislator_id,
-                'speech': $speech
-            },
-            dataType: 'json'
-        }).done(function (results) {
-            //通信成功時の処理
-            alert('成功');
-        }).fail(function (err) {
-            //通信失敗時の処理
-            alert('データの同期に失敗');
-        })
-    })
-    
-});
-*/
 $(function (goodbutton) {
   //e.preventDefault();
   $('#btn-good').on('click', function (e) {
-    alert('第一段階成功');
+    var $this = $(this);
+    var $issueID = $this.parent().find('input:hidden[name="issueID"]').val();
+    alert($issueID);
     e.preventDefault();
     $.ajax({
       type: 'POST',
@@ -143,14 +117,6 @@ $(function (goodbutton) {
     });
   });
 });
-/*
-$(function() {
-    $('a#btn-good').on('click',function() {
-        alert('成功');
-        return false;
-    });
-})
-*/
 
 /***/ }),
 
--- resources/js/good.js
+++ resources/js/good.js
@@ -1,36 +1,11 @@
-/*
-$(function() {
-    $('#btn-good').click(function(e){
-        //フォームが送信された時、デフォルトの通信を止める
-        e.stopPropagation();
-        
-        //通信を行う
-        $.ajax({
-            type: 'POST',
-            url: '#',
-            data: {
-                'speechID': $speechID,
-                'status': $status,
-                'legislator_id': $legislator_id,
-                'speech': $speech
-            },
-            dataType: 'json'
-        }).done(function (results) {
-            //通信成功時の処理
-            alert('成功');
-        }).fail(function (err) {
-            //通信失敗時の処理
-            alert('データの同期に失敗');
-        })
-    })
-    
-});
-*/
 $(function(goodbutton) {
     //e.preventDefault();
     $('#btn-good').on('click',function(e) {
         
-        alert('第一段階成功')
+        const $this = $(this);
+        const $issueID = $this.parent().find('input:hidden[name="issueID"]').val();
+        alert($issueID);
+        
         e.preventDefault();
         
         $.ajax({
@@ -50,11 +25,3 @@ $(function(goodbutton) {
         })
     });
 });
-/*
-$(function() {
-    $('a#btn-good').on('click',function() {
-        alert('成功');
-        return false;
-    });
-})
-*/
