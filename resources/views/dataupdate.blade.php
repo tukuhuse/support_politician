0dbae75d9fd97c7dba663ff8ce5c328ab7433f93
EC2 Default User <ec2-user@ip-172-31-6-52.us-east-2.compute.internal>
2020-07-09 14:30:53 +0000

    importController作成

--- resources/views/dataupdate.blade.php
+++ resources/views/dataupdate.blade.php
@@ -6,18 +6,21 @@
     <h4>CSVファイルを選択して下さい</h4>
     {{ Form::open(['method'=>'POST','url'=>'#','class'=>'form-inline']) }}
         {{ csrf_field() }}
+        {{ Form::hidden('kind','1') }}
         {{ Form::Label('国会議員のデータ') }}
         {{ Form::file('legislator',['class' => 'custom-file']) }}
         {{ Form::submit('データアップロード',['class' => 'btn btn-primary']) }}
     {{ Form::close() }}
     {{ Form::open(['method'=>'POST','url'=>'#','class'=>'form-inline']) }}
         {{ csrf_field() }}
+        {{ Form::hidden('kind','2') }}
         {{ Form::Label('政党のデータ') }}
         {{ Form::file('speaker_group',['class' => 'custom-file']) }}
         {{ Form::submit('データアップロード',['class' => 'btn btn-primary']) }}
     {{ Form::close() }}
     {{ Form::open(['method'=>'POST','url'=>'#','class'=>'form-inline']) }}
         {{ csrf_field() }}
+        {{ Form::hidden('kind','3') }}
         {{ Form::Label('選挙区のデータ') }}
         {{ Form::file('constituency',['class' => 'custom-file']) }}
         {{ Form::submit('データアップロード',['class'=> 'btn btn-primary']) }}
